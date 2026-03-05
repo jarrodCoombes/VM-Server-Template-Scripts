7a2996d31aefbfcdc395825bb9629d8c6a23f118
Jarrod Coombes <emailjrc+github@gmail.com>
2026-03-05 13:35:24 -0800

    Updated the pull script

--- 0-update_scripts.sh
+++ 0-update_scripts.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+echo +------------------------------------+
+echo Pulling latest Disk Expansion Script
+curl -s -O https://raw.githubusercontent.com/jarrodCoombes/VM-Server-Template-Scripts/master/0-update_scripts.sh 
+echo +------------------------------------+
+
+echo
+echo
+
 
 echo +------------------------------------+
 echo Pulling latest Disk Expansion Script
 curl -s -O https://raw.githubusercontent.com/jarrodCoombes/VM-Server-Template-Scripts/master/1-expand_disk.sh 
 echo +------------------------------------+
+
 
 echo
 echo
@@ -21,7 +30,8 @@ echo +------------------------------------+
 echo
 echo
 
-echo Making both scripts executable
+echo Making scripts executable
+chmod +x 0-update_scripts.sh
 chmod +x 1-expand_disk.sh
 chmod +x 2-reset_ssh_keys
 chmod +x 3-Set_HostName_Networking.sh
