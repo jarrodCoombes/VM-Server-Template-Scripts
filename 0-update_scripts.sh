03c8a7607cc50604562db4df8cdbab9e2ff09933
Jarrod Coombes <emailjrc+github@gmail.com>
2026-03-05 13:26:40 -0800

    Updated the pull script

--- 0-update_scripts.sh
+++ 0-update_scripts.sh
@@ -13,9 +13,18 @@ echo Pulling latest SSH Key Reset Script
 curl -s -O https://raw.githubusercontent.com/jarrodCoombes/VM-Server-Template-Scripts/master/2-reset_ssh_keys
 echo +------------------------------------+
 
+echo +------------------------------------+
+echo Pulling latest Host Name and Network Script
+curl -s -O https://raw.githubusercontent.com/jarrodCoombes/VM-Server-Template-Scripts/master/3-Set_HostName_Networking.sh
+echo +------------------------------------+
+
 echo
 echo
 
 echo Making both scripts executable
 chmod +x 1-expand_disk.sh
 chmod +x 2-reset_ssh_keys
+chmod +x 3-Set_HostName_Networking.sh
+
+echo Done
+
